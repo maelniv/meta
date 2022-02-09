02dbecae8bbf673cfb37730e9d92aaa7840077e6
mael314 <mael.nivel@gmail.com>
2022-02-09 11:51:55 +0100

    Update meta6.py

--- meta6.py
+++ meta6.py
@@ -3,6 +3,10 @@ import numpy as np
 import random
 import copy 
 
+#Deux parents -> Deux enfants
+#On prend le meilleur parent et le meilleur enfant
+#Ensuite on remplace dans la population de base les 50 nouveaux frérot en gardant les 3 meilleurs de l'ancienne génération (élitisme)
+
 DATA = pd.read_csv("TSPDataset/gr17.2085.tsp",header=None,delim_whitespace=True)
 SIZE_TOURNAMENT = 50
 NUMBER_TOURNAMENT_FOR_ONE_GENERATION = 50
@@ -17,10 +21,10 @@ Création d'un objet Solution pour stocker :
 - Si la solution est à priorisé pour l'élitisme
 """
 class Solution:
-    def __init__(self, city_list, distance, priority):
+    def __init__(self, city_list, distance):
         self.__city_list = city_list
         self.__distance = distance
-        self.__priority = priority
+        self.__priority = 0
 
     def get_priority(self):
         return self.__priority
@@ -59,25 +63,28 @@ class Generation:
         for i in self.get_solution_list:
             i.set_priority(0)
 
-#Deux parents -> Deux enfants
-#On prend le meilleur parent et le meilleur enfant
-#Ensuite on remplace dans la population de base les 50 nouveaux frérot en gardant les 3 meilleurs de l'ancienne génération (élitisme)
-
-
-#On crée notre population de 100 individus
+"""
+Instanciation des solutions avec leurs trajet et leurs distance calculé
+"""
 def new_random_solution():
     city_list = np.arange(1,NUMBER_CITIES)
     np.random.shuffle(city_list)
     city_list = city_list.tolist()
-    solution = Solution(city_list, find_distance_for_one_solution(city_list), 0)
+    solution = Solution(city_list, find_distance_for_one_solution(city_list))
     return solution
-    
+
+"""
+Création de la première génération de 100 individus
+"""
 def build_random_population():
     population = []
     while len(population) != SIZE_POPULATION:
         population.append(new_random_solution())
     return population
 
+"""
+Calculer la fonction distance d'une solution
+"""
 def find_distance_for_one_solution(city_list):
     distance = 0
     for city in city_list:
@@ -85,26 +92,37 @@ def find_distance_for_one_solution(city_list):
         distance += DATA[city][city_list[index_next_city]]
     return distance
 
-#On lance notre tournoirs avec 50 individus
+"""
+Initialisation du tournoi avec une partie de la population (SIZE_TOURNAMENT)
+"""
 def initialize_tournament(population):
-    tournament_schedule = []
-    while len(tournament_schedule) != SIZE_TOURNAMENT:
-        size_population_remaining = len(population)
-        chosen_solution = random.randint(0,size_population_remaining - 1)
-        tournament_schedule.append(population[chosen_solution])
-        population.pop(chosen_solution)
+    tournament_schedule = get_new_list_from_other_list_with_alea(population, SIZE_TOURNAMENT)
     return tournament_schedule
 
+"""
+Récupération du gagnant du tournoi parmis 3 finalistes tiré aléatoirement
+"""
 def find_tournament_winner(tournament_schedule):
-    picked_solution=[]
-    while len(picked_solution) != NUMBER_FINALIST:
-        size = len(tournament_schedule)
-        choice = random.randint(0, size-1)
-        picked_solution.append(tournament_schedule[choice])
-        tournament_schedule.pop(choice)
+    picked_solution = get_new_list_from_other_list_with_alea(tournament_schedule, NUMBER_FINALIST)
     picked_solution.sort(key=lambda x: x.get_distance(), reverse=True)
     return picked_solution[0]
 
+"""
+Fonction pour créer une nouvelle liste aléatoirement en partant d'une liste définie
+La nouvelle liste générée est sans doublon
+"""
+def get_new_list_from_other_list_with_alea(list, size_new_population):
+    new_list = []
+    while len(list) != size_new_population:
+        size_population_remaining = len(list)
+        index_chosen_solution = random.randint(0,size_population_remaining - 1)
+        new_list.append(list[index_chosen_solution])
+        list.pop(index_chosen_solution)
+    return new_list
+
+"""
+MAIN
+"""
 def start_genetique():
     population = build_random_population()
     list_generation = []
@@ -116,10 +134,6 @@ def start_genetique():
         tournament_schedule = initialize_tournament(copy.deepcopy(population))
         list_of_winner.append(find_tournament_winner(tournament_schedule))
     print(len(list_of_winner))
-
-
-    # distance_population=evaluation(population)
-    # tournament_winner=tournament(population,distance_population)
     
 if __name__ == '__main__':
     start_genetique()
